ee5fad3a0b5c0075fe08aafdad885c3271916871
S1120104 <S1120104@DMHS.KH.EDU.TW>
2025-09-22 03:21:07 +0000

    作業2

--- test1/main.cpp
+++ test1/main.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
+#include <iomanip> // 引入 iomanip 庫以控制輸出格式
+
 int main() {
-   std::cout << "Hello, World!\n"; // 螢幕印出字串，\n 是換行
-   return 0;                        // 回傳 0 代表「程式順利結束」
+    float fahrenheit, celsius;
+
+    // 輸入華氏溫度
+    std::cout << "請輸入華氏溫度 (F)：";
+    std::cin >> fahrenheit;
+
+    // 華氏轉攝氏
+    celsius = (fahrenheit - 32) * 5 / 9;
+
+    // 輸出結果，保留兩位小數
+    std::cout << std::fixed << std::setprecision(2); // 設定輸出格式
+    std::cout << "攝氏溫度 (C)：" << celsius << std::endl;
+
+    return 0;
 }
+
+
+   
+               
+
 
